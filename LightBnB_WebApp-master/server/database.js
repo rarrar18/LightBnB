dbaace92e1664c5ad6ab942f31d5fedda9d9eaad
Raphaella Robles <raphaella.robles@gmail.com>
2021-07-23 13:21:16 +0000

    Update getAllReservations fn to use SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,12 +83,15 @@ const getAllReservations = function(guest_id, limit = 10) {
   JOIN properties ON reservations.property_id = properties.id
   JOIN property_reviews ON properties.id = property_reviews.property_id 
   WHERE reservations.guest_id = $1
-  AND reservations.end_date < now()::date
   GROUP BY properties.id, reservations.id
+  HAVING end_date < now()::date
   ORDER BY reservations.start_date
-  LIMIT $2;`;
-  console.log(guest_id);
-  return pool.query(queryString, values).then((res) => res.rows);
+  LIMIT $2;
+  `;
+  return pool
+    .query(queryString, values)
+    .then((res) => res.rows)
+    .catch((err) => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
